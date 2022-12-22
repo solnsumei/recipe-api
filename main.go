2a2456bd595f663ab1e2d5ddc120e206314ef93c
Solomon Nsumei <solnsumei@gmail.com>
2022-12-22 08:33:23 +0100

    add redis client

--- main.go
+++ main.go
@@ -19,11 +19,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v9"
 	"github.com/solnsumei/recipe-api/config"
 	"github.com/solnsumei/recipe-api/handlers"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -49,8 +51,15 @@ func init() {
 	collection := client.Database(os.Getenv(
 		"MONGO_DATABASE")).Collection("recipes")
 
-	// SeedDB(ctx, collection) // To be commented out after seeding
+	// SeedDB(ctx, collection) // comment out after seeding
 	recipeHandler = handlers.NewRecipesHandler(ctx, collection)
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_URL"),
+		Password: "",
+		DB:       0,
+	})
+	status := redisClient.Ping(ctx)
+	fmt.Println(status)
 }
 
 func main() {
