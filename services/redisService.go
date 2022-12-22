4795ecf43152073ca4bd9a20eaa1f27393f34231
Solomon Nsumei <solnsumei@gmail.com>
2022-12-22 08:46:48 +0100

    attach redis client to recipehandler struct

--- services/redisService.go
+++ services/redisService.go
@@ -1,5 +1,21 @@
 package services
 
-func InitRedis() {
+import (
+	"context"
+	"fmt"
+	"os"
 
+	"github.com/go-redis/redis/v9"
+)
+
+func InitRedis(ctx context.Context) *redis.Client {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_URL"),
+		Password: "",
+		DB:       0,
+	})
+	status := redisClient.Ping(ctx)
+	fmt.Println(status)
+
+	return redisClient
 }
