cc560e5e6162cee3fcf32ad70b31cea5e9f652d5
Solomon Nsumei <solnsumei@gmail.com>
2022-12-22 04:33:59 +0100

    create handler and models struct

---
+++ handlers/handler.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type RecipesHandler struct {
+	collection *mongo.Collection
+	ctx        context.Context
+}
+
+func NewRecipesHandler(ctx context.Context, collection *mongo.Collection) *RecipesHandler {
+	return &RecipesHandler{
+		collection: collection,
+		ctx:        ctx,
+	}
+}
