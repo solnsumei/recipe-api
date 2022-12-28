a68ab9a0c108a27fdefbba82c72af61e6b9a34f3
Solomon Nsumei <solnsumei@gmail.com>
2022-12-28 16:57:59 +0100

    add handler for signing in users

--- handlers/auth.go
+++ handlers/auth.go
@@ -1,17 +1,20 @@
 package handlers
 
 import (
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/solnsumei/recipe-api/models"
 )
 
 type AuthHandler struct{}
 
 type Claims struct {
 	Username string `json:"username"`
-	jwt.StandardClaims
+	jwt.RegisteredClaims
 }
 
 type JWTOutput struct {
@@ -20,5 +23,38 @@ type JWTOutput struct {
 }
 
 func (handler *AuthHandler) SignInHandler(c *gin.Context) {
+	var user models.User
 
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if user.Username != "admin" || user.Password != "password" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username an/or password"})
+		return
+	}
+
+	expirationTime := time.Now().Add(10 * time.Minute)
+	claims := &Claims{
+		Username: user.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	jwtOutput := JWTOutput{
+		Token:   tokenString,
+		Expires: expirationTime,
+	}
+
+	c.JSON(http.StatusOK, jwtOutput)
 }
